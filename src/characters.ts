a4c4f2a040bb2e606224bc477b7681170c083a4b
Covadonga M <cova.millan@gmail.com>
2023-01-31 20:12:05 +0100

    added characters array, css and favicon

--- src/characters.ts
+++ src/characters.ts
@@ -3,3 +3,16 @@ import { Counselor } from './models/counselor';
 import { Fighter } from './models/fighter';
 import { King } from './models/king';
 import { Squire } from './models/squire';
+
+export const allCharacters: Character[] = [
+  new King('Joffrey', 'Baratheon', 3, 6),
+  new Fighter('Jaime', 'Lannister', 23, 'hand', 9),
+  new Fighter('Daenerys', 'Targaryen', 12, 'dragon', 3),
+];
+
+allCharacters.push(
+  new Counselor('Tyrion', 'Lannister', 34, allCharacters[3].name)
+);
+allCharacters.push(
+  new Squire('Bronn', 'Misterio', 32, allCharacters[2].name, 1)
+);
